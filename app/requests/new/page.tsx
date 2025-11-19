94c976ae80a8576d110134517491162c2b618b9f
Tonkla W. <t0nk14.wib@gmail.com>
2025-11-19 20:15:11 +0700

    refactor: new request

--- app/requests/new/page.tsx
+++ app/requests/new/page.tsx
@@ -1,5 +1,20 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
 import NewRequestForm from "../../../components/Request/NewRequestForm";
 
 export default function NewRequestPage() {
-  return <NewRequestForm />;
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        maxWidth: { sm: "100%", md: "1700px" },
+      }}
+    >
+      <Typography variant="h4" gutterBottom>
+        New Request
+      </Typography>
+      <NewRequestForm />
+    </Box>
+  );
 }
